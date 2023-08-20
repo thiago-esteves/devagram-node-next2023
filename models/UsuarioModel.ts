503140280252131c36dee6c60ffd084da697f71a
thiago-esteves <testeves.mao@gmail.com>
2023-08-20 20:38:28 -0300

    pesquisa por id

--- models/UsuarioModel.ts
+++ models/UsuarioModel.ts
@@ -1,10 +1,10 @@
 import mongoose, {Schema} from 'mongoose';
 
 const UsuarioSchema = new Schema({
-    nome:{type: String , required :true},
+    nome:{type: String, required :true},
     email :{type: String , required :true},
     senha:{type: String , required :true},
-    avatar:{type: String , required :false},
+    avatar:{type: String, required :false},
     seguidores:{type: Number , default :0},
     seguindo:{type: Number , default :0},
     publicacoes:{type: Number , default: 0},
--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -10,23 +10,33 @@ const pesquisaEnpoit
         try {
 
             if (req.method === 'GET') {
-                const { filtro } = req.query;
-                if (filtro && filtro.length < 2) {
-                    return res.status(400).json({ erro: 'metodo informado nao valido ' })
+                if(req?.query?.id) {
+                    const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id)
+                    if (!usuarioEncontrado) {
+                        return res.status(400).json({ erro: 'usuario nao encontrado ' })
+                    }
+                    usuarioEncontrado.senha =null;
+                    return res.status(200).json(usuarioEncontrado);
+               } } else {
+
+                    const { filtro } = req.query;
+                    if (filtro && filtro.length < 2) {
+                        return res.status(400).json({ erro: 'metodo informado nao valido ' })
+                    }
+                    const usuariosEncontrados = await UsuarioModel.find({
+                        $or: [{ nome: { $regex: filtro, $options: 'i' } }]
+
+                    });
+
+                    return res.status(200).json(usuariosEncontrados);
                 }
-                const usuariosEncontrados = await UsuarioModel.find({
-                    $or:[{nome:{$regex : filtro ,$options : 'i'}}],
-                    //{email:{$regex : filtro ,$options : 'i'}}]
-                });
-                return  res.status (200).json(usuariosEncontrados);
+                return res.status(405).json({ erro: 'favor informar pelo menos 2 caracteres para busca' })
+            }catch(e) {
+                       return res.status(500).json({ erro: 'nao foi possivel buscar usuarios: ' + e });
             }
-            return res.status(405).json({ erro: 'favor informar pelo menos 2 caracteres para busca' })
-        } catch (e) {
-            console.log(e);
-            return res.status(500).json({ erro: 'nao foi possivel buscar usuarios: ' + e })
+          
 
         }
-
-    }
-export default validarTokenJWT(conectarMongoDb(pesquisaEnpoit));
+    
+        export default validarTokenJWT(conectarMongoDb(pesquisaEnpoit));
 
